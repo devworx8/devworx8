a19ce93f325e0b0a62c1f3cb22e01836e84b57a2
EDP <edp@edudashpro>
2026-03-01 01:27:31 +0200

    Clean up Repo/Branch

--- components/soa-messaging/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * SOA Messaging Components
- * Export all types and components for Soil of Africa messaging
- */
-
-// Types
-export * from './types';
-
-// Components will be added here as they are created
